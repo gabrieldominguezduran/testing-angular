a1b26960a6f04599f6f90d0498a446916be9d93a
gabrieldominguezduran <gabrieldominguezduran@gmail.com>
2021-12-09 20:24:00 +0100

    Test category service

--- src/app/Services/category.service.spec.ts
+++ src/app/Services/category.service.spec.ts
@@ -31,6 +31,34 @@ const categoriesList: CategoryDTO[] = [
   },
 ];
 
+const singleCategory: CategoryDTO = {
+  userId: '',
+  categoryId: '1',
+  css_color: '',
+  description: '',
+  title: '',
+};
+
+const newCategory: CategoryDTO = {
+  userId: '1',
+  categoryId: '4',
+  css_color: '#E53419',
+  description: 'test category',
+  title: 'test',
+};
+
+const updateCategory: CategoryDTO = {
+  userId: '1',
+  categoryId: '4',
+  css_color: '#E53419',
+  description: 'test category updated',
+  title: 'test update',
+};
+
+interface deleteResponse {
+  affected: number;
+}
+
 describe('CategoryService', () => {
   let service: CategoryService;
 
@@ -67,5 +95,67 @@ describe('CategoryService', () => {
     expect(req.request.method).toBe('GET');
 
     req.flush(categoriesList);
+  });
+
+  it('POST method and createCategory create a new categoty', () => {
+    service
+      .createCategory({
+        userId: '1',
+        categoryId: '4',
+        css_color: '#E53419',
+        description: 'test category',
+        title: 'test',
+      })
+      .subscribe((resp: CategoryDTO) => {
+        expect(resp).toEqual(newCategory);
+      });
+
+    const req = httpMock.expectOne('http://localhost:3000/categories');
+
+    expect(req.request.method).toBe('POST');
+
+    req.flush(newCategory);
+  });
+
+  it('GET method and getCategoryById return one category', () => {
+    service.getCategoryById('1').subscribe((resp: CategoryDTO) => {
+      expect(resp).toEqual(singleCategory);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/categories/1');
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(singleCategory);
+  });
+
+  it('PUT method and updateCategory update the categoty', () => {
+    service
+      .updateCategory('4', {
+        userId: '1',
+        categoryId: '4',
+        css_color: '#E53419',
+        description: 'test category updated',
+        title: 'test update',
+      })
+      .subscribe((resp: CategoryDTO) => {
+        expect(resp).toEqual(updateCategory);
+      });
+
+    const req = httpMock.expectOne('http://localhost:3000/categories/4');
+
+    expect(req.request.method).toBe('PUT');
+
+    req.flush(updateCategory);
+  });
+
+  it('DELETE method and deleteCategory delete category', () => {
+    service.deleteCategory('1').subscribe((resp: deleteResponse) => {
+      expect(resp).toBeTruthy;
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/categories/1');
+
+    expect(req.request.method).toBe('DELETE');
   });
 });
